6c7e378a0909d3272c545ad0d4f957b4440ee1cd
sebin <sebinhan4@gmail.com>
2026-09-08 11:43:22 +0900

    add

--- star.py
+++ star.py
@@ -4,11 +4,9 @@ class Pyramid:
 
     def print_star_pyramid(self):
         for i in range(1, self.height + 1):
-            print(" " * (self.height - i) + "*" * (2 * i ))
+            print(" " * (self.height - i) + "*" * (2 * i - 1))
 
 
 height = int(input("피라미드 높이를 입력하세요: "))
 p = Pyramid(height)
 p.print_star_pyramid()
-
-#commit 
